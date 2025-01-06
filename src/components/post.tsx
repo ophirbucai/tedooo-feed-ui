548f161d9546e7c5406e72e28fef44e604f1af3b
Ophir Bucai <ophirbucai@gmail.com>
2025-01-06 17:47:39 +0200

    Remove usage of virtualized list and attempted minHeight map

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -6,93 +6,89 @@ import CommentIcon from "../assets/icons/actions/comment-outline.svg?react";
 import dayjs from "dayjs";
 import { cn } from "../lib/cn";
 import { useOnVisible } from "../hooks/useOnVisible";
-import { memo } from "react";
 
 type Props = {
 	post: PostType | null;
 	toggleLike?: () => Promise<void>;
 	onVisible?: (observer: IntersectionObserverEntry) => void;
-	style?: React.CSSProperties;
 };
 
-export const Post = memo(
-	({ post, toggleLike, onVisible, style = {} }: Props) => {
-		const ref = useOnVisible(onVisible, { threshold: 0.5 });
+export function Post({ post, toggleLike, onVisible }: Props) {
+	const ref = useOnVisible(onVisible, { threshold: 0.5 });
 
-		if (post) {
-			return (
-				<div className="post-spacer-y">
-					<article ref={ref} className="post" style={style} key={post.id}>
-						<header className="post__header">
-							<span className="post__header_avatar">
-								<Avatar src={post.avatar} size={40} />
-							</span>
-							<span className="post__header_name">
-								<span>{post.shopName}</span>
-							</span>
-							<span className="post__header_bottom">
-								<span className="post__header_bottom_username">
-									{post.username}
-								</span>
-								<span className="post__header_bottom_date">
-									&middot; {dayjs(post.date).fromNow(true)}
-								</span>
-							</span>
-						</header>
-						<div className="post__content">
-							<p>{post.text}</p>
-						</div>
-						<div className="post__images">
-							{post.images.slice(0, 2).map((src) => (
-								<img key={src} src={src} alt={`Posted by @${post.username}`} />
-							))}
-						</div>
-						<div className="post__interactions">
-							{post.likes > 0 && (
-								<div className="post__interactions_likes">
-									<LikeButton>
-										{post.likes > 0 &&
-											`${post.likes} like${post.likes > 1 ? "s" : ""}`}
-									</LikeButton>
-								</div>
-							)}
-							{post.comments > 0 && (
-								<div className="post__interactions_comments">
-									<button type="button">
-										{post.comments} comment{post.comments > 1 && "s"}
-									</button>
-								</div>
-							)}
-						</div>
-						<div className="post__actions">
-							<button
-								type="button"
-								className={cn(post.didLike && "active")}
-								onClick={toggleLike}
-							>
-								<LikeIcon />
-								Like
-							</button>
-							<button type="button">
-								<CommentIcon />
-								Comment
-							</button>
-						</div>
-					</article>
-				</div>
-			);
-		}
+	if (post) {
 		return (
-			<article className="post skeleton">
-				<header className="post__header">
-					<span className="post__header_avatar" />
-					<span className="post__header_name" />
-					<span className="post__header_bottom" />
-				</header>
-				<div className="post__content" />
-				<div className="post__images" />
-				<div className="post__interactions" />
-			</article>
+			<div className="post-spacer-y">
+				<article ref={ref} className="post" key={post.id}>
+					<header className="post__header">
+						<span className="post__header_avatar">
+							<Avatar src={post.avatar} size={40} />
+						</span>
+						<span className="post__header_name">
+							<span>{post.shopName}</span>
+						</span>
+						<span className="post__header_bottom">
+							<span className="post__header_bottom_username">
+								{post.username}
+							</span>
+							<span className="post__header_bottom_date">
+								&middot; {dayjs(post.date).fromNow(true)}
+							</span>
+						</span>
+					</header>
+					<div className="post__content">
+						<p>{post.text}</p>
+					</div>
+					<div className="post__images">
+						{post.images.slice(0, 2).map((src) => (
+							<img key={src} src={src} alt={`Posted by @${post.username}`} />
+						))}
+					</div>
+					<div className="post__interactions">
+						{post.likes > 0 && (
+							<div className="post__interactions_likes">
+								<LikeButton>
+									{post.likes > 0 &&
+										`${post.likes} like${post.likes > 1 ? "s" : ""}`}
+								</LikeButton>
+							</div>
+						)}
+						{post.comments > 0 && (
+							<div className="post__interactions_comments">
+								<button type="button">
+									{post.comments} comment{post.comments > 1 && "s"}
+								</button>
+							</div>
+						)}
+					</div>
+					<div className="post__actions">
+						<button
+							type="button"
+							className={cn(post.didLike && "active")}
+							onClick={toggleLike}
+						>
+							<LikeIcon />
+							Like
+						</button>
+						<button type="button">
+							<CommentIcon />
+							Comment
+						</button>
+					</div>
+				</article>
+			</div>
 		);
-	},
-);
+	}
+	return (
+		<article className="post skeleton">
+			<header className="post__header">
+				<span className="post__header_avatar" />
+				<span className="post__header_name" />
+				<span className="post__header_bottom" />
+			</header>
+			<div className="post__content" />
+			<div className="post__images" />
+			<div className="post__interactions" />
+		</article>
+	);
+}
--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,41 +1,29 @@
-import { useRef } from "react";
 import { Post } from "../components/post";
 import useInfiniteFeed from "../hooks/useInfiniteFeed";
-import { Virtuoso } from "react-virtuoso";
+import { useOnVisible } from "../hooks/useOnVisible";
 
 export function FeedPage() {
-	const minHeightMap = useRef(new Map<string, number>());
 	const { posts, loading, fetchMore, onVisible, toggleLike } =
 		useInfiniteFeed();
+	const loadMoreRef = useOnVisible(fetchMore);
 
 	return (
 		<div className="feed">
-			<Virtuoso
-				data={posts}
-				endReached={fetchMore}
-				itemContent={(index, post) => (
+			<div>
+				{posts.map((post, index) => (
 					<Post
+						key={post.id}
 						post={post}
 						toggleLike={() => toggleLike(index)}
-						onVisible={(observer: IntersectionObserverEntry) => {
-							onVisible(index);
-							minHeightMap.current.set(post.id, observer.target.scrollHeight);
-						}}
-						style={{ minHeight: minHeightMap.current.get(post.id) }}
+						onVisible={() => onVisible(index)}
 					/>
-				)}
-				components={{
-					Footer: () => (
-						<>
-							{loading && <Post post={null} />}
-							<div className="post-spacer-y" />
-						</>
-					),
-					EmptyPlaceholder: () => loading && <Post post={null} />,
-				}}
-				increaseViewportBy={{ top: 2000, bottom: 800 }}
-				useWindowScroll
-			/>
+				))}
+				{loading && <Post post={null} />}
+				<div
+					className="feed-spacer-y"
+					ref={loadMoreRef as React.LegacyRef<HTMLDivElement>}
+				/>
+			</div>
 		</div>
 	);
 }
